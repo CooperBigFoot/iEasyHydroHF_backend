39aaa5b72e2b6a0b5b3343d742ac354388dd76f2
Davor Škalec <davor.skalec@encode.hr>
2023-06-25 19:37:28 +0200

    few organization tests

--- conftest.py
+++ conftest.py
@@ -1,19 +1,33 @@
 import pytest
+import pytz
 from django.contrib.auth import get_user_model
 from django.test import Client
 from ninja_jwt.tokens import AccessToken
+
+from sapphire_backend.organizations.models import Organization
+from sapphire_backend.users.tests.factories import UserFactory
 
 User = get_user_model()
 
 
 @pytest.fixture
-def regular_user(db, user_factory):
+def regular_user(db, user_factory=UserFactory):
     return user_factory.create()
 
 
 @pytest.fixture
-def organization_admin(db, user_factory):
-    return user_factory.create(user_role=User.UserRoles.ORGANIZATION_ADMIN)
+def organization(db, organization_factory):
+    return organization_factory.create(
+        name="Kyrgyz Hydromet",
+        language=Organization.Language.RUSSIAN,
+        year_type=Organization.YearType.HYDROLOGICAL,
+        timezone=pytz.timezone("Asia/Bishkek"),
+    )
+
+
+@pytest.fixture
+def organization_admin(db, user_factory, organization):
+    return user_factory.create(user_role=User.UserRoles.ORGANIZATION_ADMIN, organization=organization)
 
 
 @pytest.fixture
@@ -29,5 +43,19 @@ def api_client():
 @pytest.fixture
 def authenticated_regular_user_api_client(regular_user):
     token = AccessToken.for_user(regular_user)
+    client = Client(HTTP_AUTHORIZATION=f"Bearer {token}")
+    return client
+
+
+@pytest.fixture
+def authenticated_organization_user_api_client(organization_admin):
+    token = AccessToken.for_user(organization_admin)
+    client = Client(HTTP_AUTHORIZATION=f"Bearer {token}")
+    return client
+
+
+@pytest.fixture
+def authenticated_superadmin_user_api_client(superadmin):
+    token = AccessToken.for_user(superadmin)
     client = Client(HTTP_AUTHORIZATION=f"Bearer {token}")
     return client
---
+++ sapphire_backend/organizations/tests/test_api.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+class TestOrganizationsAPIController:
+    endpoint = "/api/v1/organizations"
+
+    @pytest.mark.django_db
+    def test_get_single_organization_for_anonymous_user(self, organization, api_client):
+        response = api_client.get(f"{self.endpoint}/{organization.id}")
+
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Unauthorized"
+
+    @pytest.mark.django_db
+    def test_get_organizations_for_anonymous_user(self, api_client):
+        response = api_client.get(self.endpoint)
+
+        assert response.status_code == 401
+        assert response.json()["detail"] == "Unauthorized"
+
+    def test_get_organization_for_other_organization_admin(self, authenticated_regular_user_api_client, organization):
+        response = authenticated_regular_user_api_client.get(f"{self.endpoint}/{organization.id}")
+
+        assert response.status_code == 403
+        assert response.json()["detail"] == "You do not have permission to perform this action."
