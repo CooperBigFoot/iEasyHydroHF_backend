f909d642a7163bef0235f0ea7ba7c9b0c50e730e
Davor Škalec <davor.skalec@encode.hr>
2023-08-04 14:06:26 +0200

    user property tests

--- sapphire_backend/users/tests/test_models.py
+++ sapphire_backend/users/tests/test_models.py
@@ -10,3 +10,30 @@ class TestUserModelController:
 
     def test_display_name_for_user_with_only_last_name(self, user_with_only_last_name):
         assert user_with_only_last_name.display_name == "my_display_name"
+
+    def test_is_admin_for_regular_user(self, regular_user):
+        assert regular_user.is_admin is False
+
+    def test_is_admin_for_organization_admin(self, organization_admin):
+        assert organization_admin.is_admin
+
+    def test_is_admin_for_super_admin(self, superadmin):
+        assert superadmin.is_admin
+
+    def test_is_organization_admin_for_regular_user(self, regular_user):
+        assert regular_user.is_organization_admin is False
+
+    def test_is_organization_admin_for_organization_admin(self, organization_admin):
+        assert organization_admin.is_organization_admin
+
+    def test_is_organization_admin_for_super_admin(self, superadmin):
+        assert superadmin.is_organization_admin is False
+
+    def test_is_super_admin_for_regular_user(self, regular_user):
+        assert regular_user.is_superadmin is False
+
+    def test_is_super_admin_for_organization_admin(self, organization_admin):
+        assert organization_admin.is_superadmin is False
+
+    def test_is_super_admin_for_super_admin(self, superadmin):
+        assert superadmin.is_superadmin
