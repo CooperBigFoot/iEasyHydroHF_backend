a95be5a83f708e1ad997baabb15c10b54fb50b9c
Davor Škalec <davor.skalec@encode.hr>
2023-09-20 15:01:15 +0200

    Telegram parsing

--- sapphire_backend/telegrams/parser.py
+++ sapphire_backend/telegrams/parser.py
@@ -1,4 +1,9 @@
 from abc import ABC, abstractmethod
+from datetime import datetime as dt
+from datetime import timedelta
+from zoneinfo import ZoneInfo
+
+from django.conf import settings
 
 from sapphire_backend.stations.models import Station
 from sapphire_backend.telegrams.exceptions import (
@@ -83,7 +88,14 @@ class KN15TelegramParser(BaseTelegramParser):
 
         return {"section_zero": section_zero, "section_one": section_one}
 
-    def parse_section_zero(self):
+    @staticmethod
+    def adjust_water_level_value_for_negative(value: int) -> int:
+        """
+        Adjusts the value if it's over 5000 to represent negative value.
+        """
+        return value if value <= 5000 else 5000 - value
+
+    def parse_section_zero(self) -> dict[str, str | dt]:
         """
         Parses section 0 of the KN15 telegram.
         Section 0 contains the station code, date, and section code.
@@ -91,25 +103,216 @@ class KN15TelegramParser(BaseTelegramParser):
         station_code = self.get_next_token()
         self.validate_station(station_code)
 
-        # date_token = self.get_next_token()
-        # day_in_month = int(date_token[:2])
-        # hour = int(date_token[2:4])
-        # section_code = int(date_token[4])
+        def extract_day_from_token(date_token: str) -> int:
+            try:
+                day_in_month = int(date_token[:2])
+                if not (1 <= day_in_month <= 31):
+                    raise InvalidTokenException(f"Invalid day: {day_in_month}")
+            except ValueError:
+                raise InvalidTokenException(f"Invalid day: {date_token[:2]}")
 
-    def parse_section_one(self):
+            return day_in_month
+
+        def extract_hour_from_token(date_token: str) -> int:
+            try:
+                hour = int(date_token[2:4])
+                if not (0 <= hour <= 24):
+                    raise InvalidTokenException(f"Invalid hour: {hour}")
+            except ValueError:
+                raise InvalidTokenException(f"Invalid hour: {date_token[2:4]}")
+
+            return hour
+
+        def determine_date(day_in_month: int, hour: int) -> dt:
+            today = dt.now(tz=ZoneInfo(settings.TIME_ZONE))
+            current_year, current_month = today.year, today.month
+
+            # try setting the day of the current month to day_in_month
+            try:
+                parsed_date = dt(current_year, current_month, day_in_month, hour)
+            except ValueError:
+                # if day_in_month is invalid for the current month,
+                # set parsed_date to the last day of the previous month
+                if current_month == 1:  # if January, move to December of the previous year
+                    parsed_date = dt(current_year - 1, 12, 31, hour)
+                else:
+                    last_day_prev_month = (dt(current_year, current_month, 1) - timedelta(days=1)).day
+                    parsed_date = dt(current_year, current_month - 1, last_day_prev_month, hour)
+
+            # if parsed_date is in the future, move it to the previous month
+            if parsed_date > today:
+                if parsed_date.month == 1:  # if January, move to December of the previous year
+                    parsed_date = parsed_date.replace(year=parsed_date.year - 1, month=12)
+                else:
+                    parsed_date = parsed_date.replace(month=parsed_date.month - 1)
+
+            # set the time to the given hour and zero out minutes, seconds and microseconds
+            parsed_date = parsed_date.replace(minute=0, second=0, microsecond=0)
+
+            return parsed_date
+
+        input_token = self.get_next_token()
+        parsed_day_in_month = extract_day_from_token(input_token)
+        parsed_hour = extract_hour_from_token(input_token)
+        date = determine_date(parsed_day_in_month, parsed_hour)
+
+        try:
+            section_code = int(input_token[4])
+        except ValueError:
+            raise InvalidTokenException(f"Invalid hour: {input_token[4]}")
+
+        return {"station_code": station_code, "date": date, "section_code": section_code}
+
+    def parse_section_one(self) -> dict:
         """
         Parses section 1 of the KN15 telegram.
         """
-        pass
 
-    def parse_section_three(self):
+        def extract_water_level(token: str, starting_character: str) -> int:
+            if not token.startswith(starting_character):
+                raise InvalidTokenException(f"Expected token starting with '{starting_character}', got: {token}")
+            water_level = self.adjust_water_level_value_for_negative(int(token[1:]))
+
+            return water_level
+
+        def extract_water_level_trend(token: str) -> int:
+            if not token.startswith("2"):
+                raise InvalidTokenException(f"Expected token starting with '2', got: {token}")
+
+            trend = int(token[1:4])
+            trend_sign = int(token[4])
+            if trend_sign == 2:  # trend is negative
+                trend = -trend
+
+            return trend
+
+        def extract_water_and_air_temperatures(token: str) -> tuple[float, int]:
+            water_temp = int(token[1:3]) / 10
+            air_temp = int(token[3:])
+
+            if air_temp > 50:
+                air_temp = air_temp - 50
+                air_temp = -air_temp
+
+            return water_temp, air_temp
+
+        def extract_ice_phenomena(token: str) -> dict[str, int]:
+            ice_phenomena_code = int(token[1:3])
+            intensity = int(token[3:])
+            if ice_phenomena_code == intensity:
+                return {"code": ice_phenomena_code}
+            else:
+                return {"code": ice_phenomena_code, "intensity": intensity}
+
+        # water level at 08:00
+        input_token = self.get_next_token()
+        morning_water_level = extract_water_level(input_token, "1")
+
+        # water level trend
+        input_token = self.get_next_token()
+        water_level_trend = extract_water_level_trend(input_token)
+
+        # water level at 20:00
+        input_token = self.get_next_token()
+        evening_water_level = extract_water_level(input_token, "3")
+
+        # water and air temperatures - optional
+        water_temperature, air_temperature = None, None
+        if self.tokens and self.tokens[0].startswith("4"):
+            input_token = self.get_next_token()
+            water_temperature, air_temperature = extract_water_and_air_temperatures(input_token)
+
+        # ice phenomena
+        ice_phenomena = []
+        while self.tokens and self.tokens[0].startswith("5"):
+            input_token = self.get_next_token()
+            ice_phenomena.append(extract_ice_phenomena(input_token))
+
+        return {
+            "morning_water_level": morning_water_level,
+            "water_level_trend": water_level_trend,
+            "evening_water_level": evening_water_level,
+            "water_temperature": water_temperature,
+            "air_temperature": air_temperature,
+            "ice_phenomena": ice_phenomena,
+        }
+
+    def parse_section_three(self) -> int:
         """
         Parses section 3 of the KN15 telegram.
         """
-        pass
 
-    def parse_section_six(self):
+        def extract_mean_water_level(token: str) -> int:
+            if not token.startswith("1"):
+                raise InvalidTokenException(f"Expected token starting with '1', got: {token}")
+
+            mean_water_level = self.adjust_water_level_value_for_negative(int(token[1:]))
+
+            return mean_water_level
+
+        # mean water level for the period 20:00 to 08:00
+        input_token = self.get_next_token()
+        parsed_mean_water_level = extract_mean_water_level(input_token)
+
+        # skip any additional tokens until we reach the next section or end of telegram
+        while self.tokens and not self.tokens[0].startswith("9"):
+            self.get_next_token()
+
+        return parsed_mean_water_level
+
+    def parse_section_six(self) -> dict:
         """
         Parses section 6 of the KN15 telegram.
         """
-        pass
+
+        def extract_discharge_or_free_river_area(token: str) -> int:
+            significand = int(token[2:])
+            exponent = int(token[1])
+            return significand * (10 ** (exponent - 3))
+
+        # group 966MM
+        input_token = self.get_next_token()
+        month = int(input_token[3:])
+
+        # group 1HHHH
+        input_token = self.get_next_token()
+        water_level = self.adjust_water_level_value_for_negative(int(input_token[1:]))
+
+        # group 2kQQQ
+        input_token = self.get_next_token()
+        discharge = extract_discharge_or_free_river_area(input_token)
+
+        # group 3kFFF (optional)
+        free_river_area = None
+        if self.tokens and self.tokens[0].startswith("3"):
+            input_token = self.get_next_token()
+            free_river_area = extract_discharge_or_free_river_area(input_token)
+
+        # group 4hhhh (optional)
+        maximum_depth = None
+        if self.tokens and self.tokens[0].startswith("4"):
+            token = self.get_next_token()
+            maximum_depth = int(token[1:])
+
+        # group 5YYGG
+        token = self.get_next_token()
+        day_in_month = int(token[1:3])
+        hour = int(token[3:])
+
+        # Calculate the date
+        today = dt.now(tz=ZoneInfo(settings.TIME_ZONE))
+        date = dt(today.year, month, day_in_month, hour)
+        if date > today:
+            date = date.replace(year=date.year - 1)
+
+        # Skip additional tokens until section end
+        while self.tokens and not self.tokens[0].startswith("9"):
+            self.get_next_token()
+
+        return {
+            "water_level": water_level,
+            "discharge": discharge,
+            "free_river_area": free_river_area,
+            "maximum_depth": maximum_depth,
+            "date": date,
+        }
