a60b4f4b151929512c1e57e3bd2c682a2298288b
Davor Škalec <davor.skalec@encode.hr>
2023-10-03 14:58:55 +0200

    working commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -237,6 +237,9 @@ NINJA_JWT = {
 }
 
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "0.0.0.0", "127.0.0.1", "ieasyhydro.localhost"])
+
 # corsheaders
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
