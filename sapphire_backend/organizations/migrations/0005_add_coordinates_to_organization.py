33710c82f089f99ce28f746b7c017e9d8aa31bad
Davor Škalec <davor.skalec@encode.hr>
2023-10-24 14:16:48 +0200

    rename migration

--- sapphire_backend/organizations/migrations/0005_add_coordinates_to_organization.py
+++ sapphire_backend/organizations/migrations/0005_add_coordinates_to_organization.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("organizations", "0003_organization_uuid_index"),
+        ("organizations", "0004_alter_organization_language"),
     ]
 
     operations = [
