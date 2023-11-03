fb3e0407984afe2bbe9e66360a78446fb16e93cd
vatrocelan <vatro.office@gmail.com>
2023-11-03 16:46:08 +0100

    fixed test

--- sapphire_backend/organizations/tests/test_api.py
+++ sapphire_backend/organizations/tests/test_api.py
@@ -18,13 +18,9 @@ class TestOrganizationsAPIController:
         assert response.status_code == 401
         assert response.json()["detail"] == "Unauthorized"
 
-    # TODO fix and rename the test. This test doesn't do what the name says and it doesn't work.
-    #  Currently it checks if a regular user which is a member of an organization can
-    #  get the details of his organization and should assert 200.
-    #  To get 403 a new test needs to be made.
-    # def test_get_organization_for_other_organization_admin(self, authenticated_regular_user_api_client,
-    # organization):
-    #     response = authenticated_regular_user_api_client.get(f"{self.endpoint}/{organization.uuid}")
+    def test_get_organization_for_other_organization(self, authenticated_regular_user_api_client,
+                                                           other_organization):
+        response = authenticated_regular_user_api_client.get(f"{self.endpoint}/{other_organization.uuid}")
 
-    #     assert response.status_code == 403
-    #     assert response.json()["detail"] == "You do not have permission to perform this action."
+        assert response.status_code == 403
+        assert response.json()["detail"] == "You do not have permission to perform this action."
