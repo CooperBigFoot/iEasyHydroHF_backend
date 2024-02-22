c36376339ab7fc68ceb74bb65dfa30f3596d68a1
vatrocelan <vatro.office@gmail.com>
2024-02-22 13:01:30 +0100

    pre-commit

--- sapphire_backend/estimations/apps.py
+++ sapphire_backend/estimations/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class EstimationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sapphire_backend.estimations'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sapphire_backend.estimations"
--- sapphire_backend/estimations/models.py
+++ sapphire_backend/estimations/models.py
@@ -1,7 +1,4 @@
-import logging
-
-from django import db
-from django.db import connection, models
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from sapphire_backend.utils.mixins.models import UUIDMixin
