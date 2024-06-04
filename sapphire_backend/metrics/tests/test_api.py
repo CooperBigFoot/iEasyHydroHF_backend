93fe9448b0e5ad89cf75879a58f7fc33323a6af5
Davor Škalec <davor.skalec@encode.hr>
2024-06-04 07:35:26 +0200

    test tweaks

--- sapphire_backend/metrics/tests/test_api.py
+++ sapphire_backend/metrics/tests/test_api.py
@@ -84,8 +84,8 @@ class TestHydroMetricsAPI:
         response = authenticated_regular_user_api_client.get(
             self.endpoint.format(organization.uuid),
             {
-                "metric_name__in": HydrologicalMetricName.WATER_LEVEL_DAILY.value,
-                "value_type__in": HydrologicalMeasurementType.MANUAL.value,
+                "metric_name__in": HydrologicalMetricName.WATER_LEVEL_DAILY,
+                "value_type__in": HydrologicalMeasurementType.MANUAL,
                 "station__station_code": manual_hydro_station.station_code,
             },
         )
@@ -98,6 +98,7 @@ class TestHydroMetricsAPI:
                 "value_type": water_level_manual_other.value_type,
                 "sensor_identifier": water_level_manual_other.sensor_identifier,
                 "station_id": manual_hydro_station.id,
+                "value_code": None,
             },
             {
                 "avg_value": water_level_manual.avg_value,
@@ -106,6 +107,7 @@ class TestHydroMetricsAPI:
                 "value_type": water_level_manual.value_type,
                 "sensor_identifier": water_level_manual.sensor_identifier,
                 "station_id": manual_hydro_station.id,
+                "value_code": None,
             },
         ]
 
@@ -127,8 +129,8 @@ class TestHydroMetricsAPI:
         )
 
         assert response.json() == [
-            {"metric_name": HydrologicalMetricName.WATER_DISCHARGE_DAILY, "metric_count": 1},
             {"metric_name": HydrologicalMetricName.WATER_LEVEL_DAILY, "metric_count": 2},
+            {"metric_name": HydrologicalMetricName.WATER_DISCHARGE_DAILY, "metric_count": 1},
         ]
 
     def test_get_hydro_metric_count_total_only(
