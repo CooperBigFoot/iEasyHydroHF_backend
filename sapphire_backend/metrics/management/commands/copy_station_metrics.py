bdd8c22986dce4b042e71486374f3eef4622baf0
Davor Škalec <davor.skalec@encode.hr>
2024-10-07 14:32:57 +0200

    data anonymizer

--- sapphire_backend/metrics/management/commands/copy_station_metrics.py
+++ sapphire_backend/metrics/management/commands/copy_station_metrics.py
@@ -1,23 +1,49 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from sapphire_backend.metrics.management.metrics_data_anonymizer import MetricsDataAnonymizer
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("--src_id", type=int, help="ID of the source station")
-        parser.add_argument("--dest_id", type=int, help="ID of the destination station")
-        parser.add_argument("--type", type=int, help="Type of station")
+        parser.add_argument("--src", type=int, help="ID of the source station", required=True)
+        parser.add_argument("--dest", type=int, help="ID of the destination station", required=True)
+        parser.add_argument("--station_type", type=str, help="Type of station", required=True)
         parser.add_argument(
-            "--start_date", type=str, help="Starting date of the copied metrics, requires YYYY-MM-DD format"
+            "--start_date",
+            type=str,
+            help="Starting date of the copied metrics, requires YYYY-MM-DD format",
+            required=True,
         )
         parser.add_argument(
             "--end_date", type=str, help="Ending date of the copied metrics, requires YYYY-MM-DD format"
         )
+        parser.add_argument("--metrics", type=str, nargs="+", help="List of metrics to copy")
+        parser.add_argument("--value_types", type=str, nargs="+", help="Types of metrics to copy")
+        parser.add_argument("--offset_factor", type=float, default=0.0, help="Offset factor for the metrics value")
+        parser.add_argument("--copy_metrics", action="store_true", default=False, help="Copy metrics")
+        parser.add_argument("--copy_curves", action="store_true", default=False, help="Copy discharge curves")
+        parser.add_argument("--copy_telegrams", action="store_true", default=False, help="Copy telegrams")
 
     def handle(self, **options):
-        _ = MetricsDataAnonymizer(
-            station_type=options["type"],
-            src_id=options["src_id"],
-            dest_id=options["dest_id"],
+        anonymizer = MetricsDataAnonymizer(
+            station_type=options["station_type"],
+            src_id=options["src"],
+            dest_id=options["dest"],
+            start_date_str=options["start_date"],
+            end_date_str=options["end_date"],
         )
+
+        if options["copy_metrics"]:
+            if not options["metrics"] or not options["value_types"]:
+                raise CommandError("'metrics' and 'value_types' options are required when copying metrics")
+            anonymizer.copy_metrics(
+                metric_names=options["metrics"],
+                value_types=options["value_types"],
+                offset_factor=options["offset_factor"],
+            )
+
+        if options["copy_curves"]:
+            anonymizer.copy_discharge_curves()
+
+        if options["copy_telegrams"]:
+            anonymizer.copy_received_telegrams()
--- sapphire_backend/metrics/management/metrics_data_anonymizer.py
+++ sapphire_backend/metrics/management/metrics_data_anonymizer.py
@@ -1,18 +1,26 @@
 from datetime import datetime
+from decimal import Decimal
 
 from dateutil.parser import parse
+from tqdm import tqdm
 
+from sapphire_backend.estimations.models import DischargeModel
 from sapphire_backend.metrics.models import HydrologicalMetric, MeteorologicalMetric
 from sapphire_backend.metrics.timeseries.query import TimeseriesQueryManager
 from sapphire_backend.stations.models import HydrologicalStation, MeteorologicalStation
+from sapphire_backend.telegrams.models import TelegramReceived
 from sapphire_backend.utils.datetime_helper import SmartDatetime
 
 
 class MetricsDataAnonymizer:
-    def __init__(self, station_type: str, src_id: int, dest_id: int):
+    def __init__(self, station_type: str, src_id: int, dest_id: int, start_date_str: str, end_date_str: str = None):
         self.station_cls = self._get_station_cls(station_type)
         self.src_station = self._get_station(src_id)
         self.dest_station = self._get_station(dest_id)
+        self.start_date = self._get_dt(start_date_str)
+        self.end_date = (
+            self._get_dt(end_date_str) if end_date_str else SmartDatetime(datetime.now(), self.src_station).local
+        )
 
     @staticmethod
     def _get_station_cls(station_type: str) -> type(HydrologicalStation) | type(MeteorologicalStation):
@@ -39,20 +47,58 @@ class MetricsDataAnonymizer:
         dt_obj = parse(str_date)
         return SmartDatetime(dt_obj, self.src_station).local
 
-    def copy_metrics(
-        self, start_date_str: str, metric_names: list[str], value_types: list[str], end_date_str: str = None
-    ):
-        start_date = self._get_dt(start_date_str)
-        end_date = (
-            self._get_dt(end_date_str) if end_date_str else SmartDatetime(datetime.now(), self.src_station).local
-        )
-        _ = TimeseriesQueryManager(
+    def copy_metrics(self, metric_names: list[str], value_types: list[str], offset_factor: float = 0.0):
+        qm = TimeseriesQueryManager(
             model=self._get_metrics_cls(),
             filter_dict={
-                "station_id": self.src_station.id,
-                "timestamp_local__gte": start_date,
-                "timestamp_local__lte": end_date,
+                "station": self.src_station.id,
+                "timestamp_local__gte": self.start_date,
+                "timestamp_local__lte": self.end_date,
                 "metric_name__in": metric_names,
                 "value_type__in": value_types,
             },
         )
+
+        metrics = qm.execute_query()
+
+        for metric_dict in tqdm(metrics.values(), total=metrics.count(), desc="Copying metrics..."):
+            cls = self._get_metrics_cls()
+            new_metric_dict = {**metric_dict, "station_id": self.dest_station.id}
+            if cls == HydrologicalMetric:
+                new_metric_dict["avg_value"] = metric_dict["avg_value"] * Decimal(1 + offset_factor)
+            else:
+                new_metric_dict["value"] = metric_dict["value"] * Decimal(1 + offset_factor)
+
+            new_metric = cls(**new_metric_dict)
+            new_metric.save()
+
+    def copy_discharge_curves(self):
+        existing_dm = self.src_station.dischargemodel_set.filter(
+            valid_from_local__range=(self.start_date, self.end_date)
+        )
+        for dm in tqdm(existing_dm.values(), total=existing_dm.count(), desc="Copying discharge curves..."):
+            new_dm_dict = {**dm, "station": self.dest_station}
+            new_dm_dict.pop("id", None)
+            new_dm = DischargeModel(**new_dm_dict)
+            new_dm.save()
+
+    def copy_received_telegrams(self):
+        existing_telegrams = TelegramReceived.objects.filter(
+            organization=self.src_station.site.organization,
+            station_code=self.src_station.station_code,
+            created_date__range=(self.start_date, self.end_date),
+        )
+
+        for tr in tqdm(existing_telegrams.values(), total=existing_telegrams.count(), desc="Copying telegrams..."):
+            new_tr_dict = {
+                **tr,
+                "station_code": self.dest_station.station_code,
+                "organization": self.dest_station.site.organization,
+                "acknowledged": False,
+                "acknowledged_by": None,
+                "acknowledged_ts": None,
+                "filestate": None,
+            }
+            new_tr_dict.pop("id", None)
+            new_tr = TelegramReceived(**new_tr_dict)
+            new_tr.save()
