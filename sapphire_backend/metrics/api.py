9e2165de7a174b1fbadb3867b29e9db6f1af2755
Davor Škalec <davor.skalec@encode.hr>
2024-10-18 14:32:18 +0200

    working commit

--- sapphire_backend/metrics/api.py
+++ sapphire_backend/metrics/api.py
@@ -14,6 +14,7 @@ from django.http import FileResponse
 from django.templatetags.static import static
 from ninja import File, Query
 from ninja.files import UploadedFile
+from ninja.pagination import PageNumberPagination, paginate
 from ninja_extra import api_controller, route
 from ninja_jwt.authentication import JWTAuth
 from zoneinfo import ZoneInfo
@@ -95,6 +96,7 @@ agg_func_mapping = {"avg": Avg, "count": Count, "min": Min, "max": Max, "sum": S
 )
 class HydroMetricsAPIController:
     @route.get("", response={200: list[HydrologicalMetricOutputSchema]})
+    @paginate(PageNumberPagination, page_size=100)
     def get_hydro_metrics(
         self,
         organization_uuid: str,
